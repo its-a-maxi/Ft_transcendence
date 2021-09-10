d9463a2c078b769114adf0a88a6ff4b14c67af1b
Maxi <mmonroy-@Maxis-iMac.local>
2021-09-10 19:28:36 +0200

    mainPage component reworked and fixed

--- frontend/src/app/components/main-page/main-page.component.ts
+++ frontend/src/app/components/main-page/main-page.component.ts
@@ -12,14 +12,6 @@ export class MainPageComponent implements OnInit {
 
   constructor(public authService: AuthService, private router: Router) { }
 
-  private tabsText: any;
-  private hamburger: any;
-  private profile: any;
-  private bottom: any;
-  private content: any;
-
-  private clicked: boolean = false;
-
   private user?: User;
   nick: string | undefined;
   name: string | undefined;
@@ -32,14 +24,29 @@ export class MainPageComponent implements OnInit {
     if (!this.user)
       this.router.navigate(['/landingPage/start']);
     this.getConnectedUsers();
-    this.tabsText = document.getElementById("tabsText");
-    this.hamburger = document.getElementById("hamburger");
-    this.profile = document.getElementById("profile");
-    this.bottom = document.getElementById("bottom");
-    this.content = document.getElementById("content");
     this.nick = this.user?.nick;
     this.name = this.user?.name;
     this.picture = this.user?.avatar;
+  }
+
+  showTabs() : void
+  {
+    let container = document.getElementById("all");
+    let pageBack = document.getElementById("pageBack")
+    let hiddenTabs = document.getElementById("hiddenTabs")
+    container!.style.opacity = "50%";
+    pageBack!.style.display = "block";
+    hiddenTabs!.style.left = "-10vh";
+  }
+
+  hideTabs() : void
+  {
+    let container = document.getElementById("all");
+    let pageBack = document.getElementById("pageBack")
+    let hiddenTabs = document.getElementById("hiddenTabs")
+    container!.style.opacity = "100%";
+    pageBack!.style.display = "none";
+    hiddenTabs!.style.left = "-100vw";
   }
 
   private async getConnectedUsers()
@@ -50,51 +57,6 @@ export class MainPageComponent implements OnInit {
     for(let i = 0; users[i]; i++)
       if (users[i].isConnected == true)
         this.liveUsers++;
-    return;
-  }
-
-  private openNav(): void
-  {
-    this.tabsText!.style.left = "0%";
-    this.hamburger!.style.backgroundImage = 'url("../../../assets/images/back.png")';
-    this.profile!.style.display = "none";
-    this.bottom!.style.display = "none";
-    this.content!.style.display = "none";
-    return;
-  }
-
-  private closeNav(): void
-  {
-    this.tabsText!.style.left = "-150%";
-    this.hamburger!.style.backgroundImage = 'url("../../../assets/images/hamburger.png")';
-    this.profile!.style.display = "block";
-    this.bottom!.style.display = "block";
-    this.content!.style.display = "block";
-    return;
-  }
-
-  public clickTab(): void
-  {
-    if (this.clicked == true)
-    {
-      this.closeNav();
-      this.clicked = false;
-    }
-    return;
-  }
-
-  public clickNav(): void
-  {
-    if (this.clicked == false)
-    {
-      this.openNav();
-      this.clicked = true;
-    }
-    else
-    {
-      this.closeNav();
-      this.clicked = false;
-    }
     return;
   }
 
