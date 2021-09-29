cc200186a3fdc0e4d6f089a39bf3083d7013d2e5
Maxi <mmonroy-@Maxis-iMac.local>
2021-09-29 10:38:19 +0200

    stylizing pong game

--- frontend/src/app/components/pong-game/pong-game.component.ts
+++ frontend/src/app/components/pong-game/pong-game.component.ts
@@ -32,8 +32,9 @@ export class PongGameComponent implements OnInit, AfterViewInit, OnDestroy
 	paddleHeight = 100;
 
 	// -> Colors
-	backgroundColor = "#000";
-	elementsColor = "#FFF";
+	backgroundColor = 'rgb(19, 5, 11)';
+	elementsColor = "#ede5f2ff";
+	scoreColor = "rgba(237, 229, 242, 0.2)";
 
 	test: any
 
@@ -136,10 +137,12 @@ export class PongGameComponent implements OnInit, AfterViewInit, OnDestroy
 		this.context!.fillStyle = this.backgroundColor;													// Sets background color.
 		this.context!.fillRect(0, 0, this.canvas.width, this.canvas.height);									// Draws background.
 		this.context!.fillStyle = this.elementsColor;														// Sets elements color.
-		this.context!.font = "35px sans-serif";														// Sets font for text elements (score).
+		this.context!.font = "20vh futura";														// Sets font for text elements (score).
 		this.context!.fillRect(this.canvas.width / 2 - this.netWidth / 2, 0, this.netWidth, this.netHeight);				// Draws net.
-		this.context!.fillText(userOne.score.toString(), this.canvas.width / 4, this.canvas.height / 6);			// Draws user score.
-		this.context!.fillText(userTwo.score.toString(), 3 * this.canvas.width / 4, this.canvas.height / 6);			// Draws ai score.
+		this.context!.fillStyle = this.scoreColor;														// Sets elements color.
+		this.context!.fillText(userOne.score.toString(), this.canvas.width / 5, this.canvas.height / 1.6);			// Draws user score.
+		this.context!.fillText(userTwo.score.toString(), 3 * this.canvas.width / 4.7, this.canvas.height / 1.6);			// Draws ai score.
+		this.context!.fillStyle = this.elementsColor;														// Sets elements color.
 		this.context!.fillRect(userOne.x, userOne.y, this.paddleWidth, this.paddleHeight);							// Draws user paddle.
 		this.context!.fillRect(userTwo.x, userTwo.y, this.paddleWidth, this.paddleHeight);								// Draws ai paddle.
 		this.context!.beginPath();																	// Draws ball.
